68cda3d3fe4fa82da49040335568ce136125b6a4
warlyware <danward@gmail.com>
2018-05-06 22:12:44 -0400

    Add working saveAlbum and GET albums endpoints

--- server/index.js
+++ server/index.js
@@ -3,11 +3,11 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-
-
 const app = express();
 
 app.use(bodyParser.json());
+
+const redirect_uri = 'http://localhost:3000';
 
 const MONGO_URI = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASS}@ds237989.mlab.com:37989/spotify-saver`;
 
@@ -17,14 +17,44 @@ mongoose.connection
   .once('open', () => console.log('Connected to MongoLab instance.'))
   .on('error', error => console.log('Error connecting to MongoLab:', error));
 
+const albumSchmea = mongoose.Schema({
+  albumId: String
+});
+
+const Album = mongoose.model('Album', albumSchmea);
+
 app.get('/', (req, res) => {
   res.send('api online');
-})
+});
 
-app.post('/api', (req, res) => {
-  console.log('heard post', req);
-  res.send(JSON.stringify(req.body));
-})
+app.get('/login', (req, res) => {
+  var scopes = 'user-read-private user-read-email';
+  res.redirect('https://accounts.spotify.com/authorize' +
+  '?response_type=code' +
+  '&client_id=' + my_client_id +
+  (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+  '&redirect_uri=' + encodeURIComponent(redirect_uri));
+});
+
+app.get('/albums', (req, res) => {
+  Album.find((err, albums) => {
+    // if (err) return console.error(err);
+    // console.log(JSON.stringify(albums));
+    // return albums;
+    res.send(albums);
+  });
+});
+
+app.post('/', (req, res) => {
+  console.log('heard post', req.body);
+  const album = new Album({
+    albumId: req.body.albumId
+  });
+  album.save().then((response) => {
+    console.log(`Saved to database: ${response}`)
+  });
+  res.send(`Saved ${JSON.stringify(req.body)}`);
+});
 
 app.listen(4000, () => {
   console.log('Server up');
--- src/App.js
+++ src/App.js
@@ -2,16 +2,51 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+
 class App extends Component {
+  state = {
+    albumId: ''
+  }
+
+  displaySavedNotification = () => {
+    console.log(`album saved: ${this.state.albumId}`);
+  }
+
+  handleInputChange = (event) => {
+    this.setState({
+      albumId: event.target.value,
+    });
+  }
+
+  saveAlbum = async () => {
+    try {
+      const response = await axios.post('/api/saveAlbum', {
+        body: {
+          albumId: this.state.albumId
+        },
+        headers: {
+          'content-type': 'application/json'
+        }
+      });
+      this.displaySavedNotification();
+    } catch (error) {
+      console.error(`problem saving album: ${this.state.albumId}`)
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Save It!</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <input type="text"
+          value={this.state.albumId}
+          onChange={this.handleInputChange} />
+          <button onClick={this.saveAlbum}>Save</button>
         </p>
       </div>
     );
